61ae8771794cb415ccc3a4db96587c5b8c7e5b2f
Given Reed <greed77@gmail.com>
2012-09-17 22:08:42 -0400

    initial commit
    reads in hosts file on win7, no other functionality yet

---
+++ src/HostsEditor/frmMain.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace HostsEditor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstHosts = new System.Windows.Forms.ListView();
+            this.lstHostsEnabled = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lstHostsAddress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lstHostsDomain = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lstHosts
+            // 
+            this.lstHosts.CheckBoxes = true;
+            this.lstHosts.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lstHostsEnabled,
+            this.lstHostsAddress,
+            this.lstHostsDomain});
+            this.lstHosts.FullRowSelect = true;
+            this.lstHosts.Location = new System.Drawing.Point(12, 12);
+            this.lstHosts.MultiSelect = false;
+            this.lstHosts.Name = "lstHosts";
+            this.lstHosts.Size = new System.Drawing.Size(469, 218);
+            this.lstHosts.TabIndex = 0;
+            this.lstHosts.UseCompatibleStateImageBehavior = false;
+            this.lstHosts.View = System.Windows.Forms.View.Details;
+            // 
+            // lstHostsEnabled
+            // 
+            this.lstHostsEnabled.Text = "";
+            // 
+            // lstHostsAddress
+            // 
+            this.lstHostsAddress.Text = "IP Address";
+            // 
+            // lstHostsDomain
+            // 
+            this.lstHostsDomain.Text = "Domain";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 428);
+            this.Controls.Add(this.lstHosts);
+            this.Name = "frmMain";
+            this.Text = "Hosts File Editor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstHosts;
+        private System.Windows.Forms.ColumnHeader lstHostsEnabled;
+        private System.Windows.Forms.ColumnHeader lstHostsAddress;
+        private System.Windows.Forms.ColumnHeader lstHostsDomain;
+    }
+}
+
---
+++ src/HostsEditor/frmMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace HostsEditor
+{
+    public partial class frmMain : Form
+    {
+        private const string FILE_NAME = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+        // private string FILE_NAME = Environment.GetFolderPath(Environment.SpecialFolder.System) + @"\drivers\etc\hosts";
+
+        public frmMain()
+        {
+            InitializeComponent();
+            ReadHosts();
+        }
+
+        public void ReadHosts()
+        {
+            lstHosts.Items.Clear();
+
+            if (!File.Exists(FILE_NAME))
+            {
+                MessageBox.Show(FILE_NAME + " does not exist.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(FILE_NAME))
+                {
+                    String input;
+                    string[] saLvwItem = new string[3];
+
+                    while ((input = sr.ReadLine()) != null)
+                    {
+                        string strVal = input.ToString();
+                        string[] lines = strVal.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (lines.Length > 1)
+                        {
+                            string[] line = lines[1].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                            if (line.Length > 1)
+                            {
+                                ListViewItem item = new ListViewItem();
+                                // item.SubItems.Add(lines[0]);
+                                item.SubItems.Add(line[0]);
+                                item.SubItems.Add(line[1]);
+                                lstHosts.Items.Add(item);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                MessageBox.Show("The file could not be read\n" + e.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public void WriteHosts()
+        {
+        }
+    }
+}
